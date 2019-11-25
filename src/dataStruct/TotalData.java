0bb6dd01edb82b07aa13763c72dd912ca6d5ab5d
StarbowBroken <719685319@qq.com>
2019-11-25 12:13:15 +0800

    Interface_Design Commit 1
    大致定义了接口

---
+++ src/dataStruct/TotalData.java
@@ -0,0 +1,33 @@
+package dataStruct;
+
+import java.util.List;
+
+/**
+ * 总数据池对象。
+ * 本程序所有内存数据均在本类中管理。
+ * 目前仅定义了主程序需要的数据，请持续完成本类定义。
+ * @author HIT_1170300119
+ *
+ */
+public class TotalData {
+  private byte[] id;                            //客户端ID，首次运行随机生成，由主程序负责生成和加载
+  private List<TransferData> TransferList;      //传输信息列表，主程序展示使用，由服务器和客户端写入，请注意线程安全
+  private List<TransferData> SharedList;        //当前已分享表，主程序展示使用，由客户端写入
+  
+  public TotalData(byte[] id,List<TransferData> TransferList) {
+    this.id=id;
+    this.TransferList=TransferList;
+  }
+  
+  public byte[] getId() {
+    return id;
+  }
+  
+  public List<TransferData> getTransferList() {
+    return TransferList;
+  }
+  
+  public List<TransferData> getSharedList() {
+    return SharedList;
+  }
+}
